6799489c014dfc4c9ba8201a575fef4103cbdb3e
Avi Lichtschein <AviLichtschein@gmail.com>
2014-07-28 17:39:54 -0400

    NOW added the Twitter api keys and consumer tokens

---
+++ config/initializers/twitter_api_info.rb
@@ -0,0 +1,6 @@
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['ppQ1p1AXbtvxC9qTR6PsfJppW']
+  config.consumer_secret = ENV['YMWmCr2oeFSdQkbxFhyS6qdI4BIuvV7iZFQxSIbyP2exIlK0Xu']
+  config.access_token = ENV['174534564-v29cIDBrl0VCQnoUyuvMCuDOmRDLHDPGpmwRNwrr']
+  config.access_token_secret = ENV['IMQJwQeEtqeR1q8AnJhRehFaVikyXdyROfQD1DJ9lcYck']
+end
