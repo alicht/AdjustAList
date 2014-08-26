63f38d3bb3b5d53a81d3df5d18c2f4bf727b23d9
Avi Lichtschein <AviLichtschein@gmail.com>
2014-08-26 15:57:26 -0400

    just about almost there...

--- app/controllers/user_added_list_controller.rb
+++ app/controllers/user_added_list_controller.rb
@@ -28,18 +28,44 @@ class UserAddedListController < ApplicationController
       config.access_token        = current_user.access_token
       config.access_token_secret = current_user.access_token_secret
     end
-    @members = twitter_client.list_members(params[:list_id].to_i) 
+    @list_id = params[:list_id].to_i 
+    @members = twitter_client.list_members(@list_id)
   end
 
-  def remove_list_member(list, user_to_remove)
-    binding.pry
+  def remove_list_members
     twitter_client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['twitter_consumer_key']
       config.consumer_secret     = ENV['twitter_consumer_secret']
       config.access_token        = current_user.access_token
       config.access_token_secret = current_user.access_token_secret
     end
+    @list_id = params[:id].to_i 
+    @users = params[:list][:usernames]
+    @remove_members = twitter_client.remove_list_members(@list_id, @users)
+    redirect_to list_members_path(@list_id)
+    @new_list = twitter_client.create_list(params[:name_of_list])
+    @add_members = twitter_client.add_list_member(@new_list, params[:user_to_add])
   end
+
+  # def create_new_list
+  #   twitter_client = Twitter::REST::Client.new do |config|
+  #     config.consumer_key        = ENV['twitter_consumer_key']
+  #     config.consumer_secret     = ENV['twitter_consumer_secret']
+  #     config.access_token        = current_user.access_token
+  #     config.access_token_secret = current_user.access_token_secret
+  #   end
+  #   @new_list = twitter_client.create_list(params[:name_of_list])
+  # end
+
+  # def add_list_members
+  #   twitter_client = Twitter::REST::Client.new do |config|
+  #     config.consumer_key        = ENV['twitter_consumer_key']
+  #     config.consumer_secret     = ENV['twitter_consumer_secret']
+  #     config.access_token        = current_user.access_token
+  #     config.access_token_secret = current_user.access_token_secret
+  #   end
+  #   @add_members = twitter_client.add_list_members(list, users)
+  # end
 
   private
 
