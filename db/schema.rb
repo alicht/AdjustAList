5e32b7d001627aa740abf870c99fb5b1922afa6d
Avi Lichtschein <AviLichtschein@gmail.com>
2014-08-28 15:12:59 -0400

    message

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140818192500) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "user_added_lists", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
