380ee39dd6abc8678c16ab580bc874a105530992
Avi Lichtschein <AviLichtschein@gmail.com>
2014-09-14 15:42:39 -0400

    cleaning up some changes

--- app/controllers/getting_a_twitter_list_controller.rb
+++ app/controllers/getting_a_twitter_list_controller.rb
@@ -1,26 +1,11 @@
 class GettingATwitterListController < ApplicationController
+  
   def index
   
   end
 
-  # def run
-  # end
-
-  # def welcome
-  # end
-
   def twitter_list
-    # @twitter_list = Twitter::REST::Client.new do |config|
-    #   config.consumer_key        = ENV['twitter_consumer_key']
-    #   config.consumer_secret     = ENV['twitter_consumer_secret']
-    #   config.access_token        = current_user.access_token
-    #   config.access_token_secret = current_user.access_token_secret
-    # end
-    # @twitter_list.subscriptions
-
-    # where stuff happens
-    # instantiate an instance of the twitter gem
-    # set up data for the view
+   
   end
     
 
