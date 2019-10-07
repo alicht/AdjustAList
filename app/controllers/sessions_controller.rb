507855f189e6aeae2bc4da98b286226ff521f22b
alicht <avilichtschein@gmail.com>
2019-10-07 09:33:19 -0400

    grab attributes manually

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
-    auth_hash = env["omniauth.auth"]
-    user = User.from_omniauth(auth_hash)
+    auth_hash = request.env["omniauth.auth"]
+
+    auth = { credentials: { token: auth_hash["credentials"]["token"], secret: auth_hash["credentials"]["secret"]}, uid: auth_hash["uid"], provider: auth_hash["provider"], info: { nickname: auth_hash["info"]["nickname"] } }
+
+    user = User.from_omniauth(auth)
     session[:user_id] = user.id
     session[:twitter_access_token] = auth_hash["credentials"]["token"]
     session[:twitter_access_token_secret] = auth_hash["credentials"]["secret"]
@@ -12,6 +15,10 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Signed out"
+  end
+
+  def user_params
+    params.require(:user).permit!
   end
 
 end
